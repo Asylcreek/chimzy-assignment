8b2b94f67ef1009c90f2e4f4766d035d2dfed43b
Omokugbo Joseph Boro <omokugbobr@gmail.com>
2024-12-17 22:36:09 +0100

    feat: Start appointment page

---
+++ pages/appointment.tsx
@@ -0,0 +1,47 @@
+import Image from "next/image";
+
+import styles from "~/styles/appointment.module.scss";
+
+const AppointmentPage = () => {
+  return (
+    <section className={styles.section}>
+      <div className={styles.detailsContainer}>
+        <h1>Book your appointment</h1>
+
+        <div>
+          <div>
+            <Image src="/images/massage-1.jpeg" alt="" fill sizes="100%" />
+          </div>
+
+          <p>Stress Reduction and Relaxation</p>
+
+          <p>
+            A potent resurfacing treatment using powerful exfoliation and
+            renewal techniques to promote smoother, younger looking skin through
+            targeting the signs of ageing and dull or uneven skin tones.
+          </p>
+
+          <div>
+            <p>$124</p>
+
+            <p>&nbsp;| 45 Minutes</p>
+          </div>
+        </div>
+      </div>
+
+      <div className={styles.calendarContainer}>
+        <h2>Choose Date and Time</h2>
+
+        <p>Selected Date:</p>
+
+        <div></div>
+
+        <button type="button">Confirm date</button>
+
+        <button type="button">Cancel</button>
+      </div>
+    </section>
+  );
+};
+
+export default AppointmentPage;
