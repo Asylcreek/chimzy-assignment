8bc3be39539fc13f938ea69381f268fe144123ea
Omokugbo Joseph Boro <omokugbobr@gmail.com>
2024-12-17 19:52:18 +0100

    feat: Add shop page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,8 @@
-import "~/styles/globals.css";
 import type { AppProps } from "next/app";
+
+import "~/styles/globals.css";
+import "~/styles/vars.css";
+import "~/styles/base.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
